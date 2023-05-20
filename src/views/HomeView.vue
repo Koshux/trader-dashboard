7e7317f5e81fc38f8bad5af1d88b905364f0979a
Koshux <lanzonprojects@gmail.com>
2023-05-20 18:22:07 +0200

    Initial commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-container style="height: 100vh;">
+    <el-aside width="200px">
+      <el-menu default-active="1" class="el-menu-vertical">
+        <el-menu-item index="1">Home</el-menu-item>
+      </el-menu>
+    </el-aside>
+    <main ref="chartContainer">
+      <TheDashboard />
+    </main>
+  </el-container>
+</template>
+
+<script setup lang="ts">
+import TheDashboard from '@/components/TheDashboard.vue'
+</script>
+
+<style scoped>
+main {
+  padding: 20px;
+  background-color: #fff;
+  overflow: auto;
+}
+
+.el-menu-vertical-demo:not(.el-menu--collapse) {
+  width: 200px;
+  min-height: 400px;
+}
+</style>
+
